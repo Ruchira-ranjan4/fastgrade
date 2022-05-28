a9a5b9dfbbea42bd1e0b1376a40edf685de7c227
Ruchira-ranjan4 <34804989+Ruchira-ranjan4@users.noreply.github.com>
2022-05-28 21:34:15 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <link rel="icon" href="images/favicon.png" type="image/gif" sizes="16x16">  
+    <title>LearnDotCom</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+    <link href="css/owl.carousel.min.css" rel="stylesheet">
+    <link href="css/owl.theme.default.min.css" rel="stylesheet">
+    <link href="css/animation.css" rel="stylesheet">
+    <link href="css/prettyPhoto.css" rel="stylesheet"> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://rawgit.com/LeshikJanz/libraries/master/Bootstrap/baguetteBox.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+<body>
+<main role="main">
+    <!-- START NAVBAR -->
+
+  <div id="myOverlay" class="overlay">
+    <span class="closebtn" onclick="closeSearch()" title="Close Overlay">×</span>
+    <div class="overlay-content">
+      <form action="/action_page.php">
+        <input type="text" placeholder="Search.." name="search">
+        <button type="submit"><i class="fa fa-search"></i></button>
+      </form>
+    </div>
+  </div>
---
+++ navbar.php
@@ -0,0 +1,40 @@
+<header>
+<?php include('header.php'); ?> 
+
+<div class="top-header" id="navbar_top">
+       <div class="container-fluid">
+           <div class="row align-items-center">
+               <div class="col-md-12 d-flex justify-content-between align-items-center"> 
+                   <div class="left-head d-flex justify-content-start align-items-center">
+                       <a class="navbrnd" href="index.php"><img src="images/logo.png"></a>
+                      
+                      
+                   </div>                   
+                   <div class="right-head d-flex justify-content-end align-items-center">
+                   <div class="dropdown subjt">
+                          <button class="dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Subject
+                          </button>
+                          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                          </div>
+                       </div>
+                       <div class="nav-mnsec">
+                           <a href="">FIND TUTORING JOBS
+</a>
+                           <a href="">FIND A TUTOR</a>
+                          
+                       </div>
+                       <div class="notif-sec">
+                           <a href="#"><img src="images/bell.png"><span></span></a>
+                       </div>
+                       
+                      
+                   </div>
+               </div>              
+           </div>
+       </div> 
+    </div>
+</header>  
